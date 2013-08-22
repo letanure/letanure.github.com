d98139cd8fbf890507d56c25c24d2aad7463f5d9
luiz Tanure <letanure@gmail.com>
2013-08-22 14:37:30 -0300

    main js

---
+++ assets/javascripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  // $('#comunidades_no_facebook').next('ul').find('a').each(function(i,link){
+  //   console.log( link.href )
+  //   $.getJSON(link.href, function(data){
+  //     console.log(data)
+  //   });
+  // })
+
+
+});
